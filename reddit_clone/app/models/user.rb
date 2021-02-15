4a0e8ccdeaf241e66f03d5aadc0f6056a4d2c1e1
Marco Torre <marco.a.torre@gmail.com>
2021-02-15 14:10:37 -0500

    Built out ApplicatioController

--- reddit_clone/app/models/user.rb
+++ reddit_clone/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, :session_token, presence: true, uniqueness: true
-    validate :password, length: {minimum: 6, allow_nil: true}
+    validates :password, length: {minimum: 6, allow_nil: true}
     validates :password_digest, presence: true
 
     attr_reader :password
@@ -14,4 +14,30 @@ class User < ApplicationRecord
         pass_check = BCrypt::Password.new(self.password_digest)
         pass_check.is_password?(password)
     end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user.is_password?(password)
+            return user
+        end
+        false
+    end
+
+    def generate_session_token
+        session_token = SecureRandom.urlsafe_base64(16)
+        session_token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    private
+
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
+
 end
